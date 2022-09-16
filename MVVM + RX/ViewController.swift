7e24e3c7492b228b08cc1da35906d81f2ceba7be
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-09-16 18:51:48 +0800

    Initial Commit

---
+++ MVVM + RX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM + RX
+//
+//  Created by Виктор Басиев on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
